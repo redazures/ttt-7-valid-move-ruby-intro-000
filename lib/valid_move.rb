df5dccb497762f426b7b4a1756046a7a44586cd9
james wu <jwu_nyc@yahoo.com>
2020-06-13 19:21:45 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,8 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
-  if position_taken?(board,index)
+  if index>8
+    false
+  elsif position_taken?(board,index)
     false
   else true
   end
